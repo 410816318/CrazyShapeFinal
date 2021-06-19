d26470f299a512d0e558b3593fedba61621acf4a
吳曜宇 <s1081631@gm.pu.edu.tw>
2021-06-20 01:12:57 +0800

    2021_上機考v3_吳曜宇 改

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -8,16 +8,21 @@ import android.widget.Toast
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 @GlideModule
 public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
+
+    var Flag:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Toast.makeText(baseContext, "作者：吳曜宇", Toast.LENGTH_LONG).show()
+        rndShape()
 
         GlideApp.with(this)
             .load(R.drawable.cover)
@@ -32,5 +37,22 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+
+        imgNext.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                rndShape()
+            }
+
+        })
+    }
+
+    fun rndShape(){
+        Flag = (1..4).random()
+        when(Flag){
+            1->imgNext.setImageResource(R.drawable.circle)
+            2->imgNext.setImageResource(R.drawable.square)
+            3->imgNext.setImageResource(R.drawable.star)
+            4->imgNext.setImageResource(R.drawable.triangle)
+        }
     }
 }
